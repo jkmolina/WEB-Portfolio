bbe832c7bf5772d8415529115f77234f7c17bf86
jkmolina <kim18096@uvg.edu.gt>
2021-06-10 02:45:12 -0600

    finish

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -2,16 +2,47 @@ import React from 'react'
 
 import img from './avatar.png'
 
-const paragraph = `Jugador seleccionado departamental volleyball Guatemala 2017
-Inicio UVG 2018
-Inicio en conversionaventa.com como representante de ventas en servicios de marketing digital para bienes raices 2018
-Inicio en markeko.com como web designer y copywriter en 2019
-Cambio de ing industrial a ing en sistemas en 2020`
+import './about.css'
+
+const fechas = [2017, 2018, 2018, 2019, 2020]
+const eventos = [
+  'Jugador seleccionado departamental de volleyball en Guatemala',
+  'Inicio de estudios en UVG en la carrera de ing. industrial',
+  'Inicio en conversionaventa.com como representante de ventas en servicios de marketing digital para bienes raices',
+  'Inicio en markeko.com como web designer y copywriter',
+  'Cambio de ing. industrial a ing. en sistemas',
+]
+
+const elementos = []
+
+for (let i = 0; i < 5; i += 1) {
+  elementos.push(
+    <li>
+      <span>{fechas[i]}</span>
+      <p>{eventos[i]}</p>
+    </li>,
+  )
+}
+
+const conocimientos = ['Marketing', 'Diseño web', 'Copywriting', 'Programación', 'Sistemas Web', 'Bases de datos'].map((str) => <li style={{ listStyle: 'square' }}>{str}</li>)
 
 const About = () => (
   <div id="about">
-    <img src={img} alt="avatar" />
-    <div><p>{paragraph}</p></div>
+    <h1>Joonho Kim</h1>
+    <hr />
+    <div className="top">
+      <img src={img} alt="avatar" />
+      <div>
+        <h2>Linea del tiempo</h2>
+        <ul className="timeline">
+          {elementos}
+        </ul>
+        <h2>Conocimientos</h2>
+        <ul className="conocimientos">
+          {conocimientos}
+        </ul>
+      </div>
+    </div>
   </div>
 )
 
--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
 
+import cssonly from './imgs/css.png'
+import jsonly from './imgs/js.png'
+import maze from './imgs/maze.png'
+import calc from './imgs/calc.png'
+import asist from './imgs/asist.png'
+
+import './projects.css'
+
+const titulos = ['Single div css image', 'Chat en JS', 'Maze in react', 'Calculadora en react', 'PWA para asistencia']
+const imagenes = [cssonly, jsonly, maze, calc, asist]
+const links = ['http://ubeje.xyz/18096/lab4/pokeball.html', 'http://ubeje.xyz/18096/lab05/', 'http://ubeje.xyz/18096/lab06/', 'http://ubeje.xyz/18096/lab7/dist/', 'https://fir-web-lab7.web.app/']
+const elementos = []
+
+for (let i = 0; i < titulos.length; i += 1) {
+  elementos.push(
+    <li>
+      <a href={links[i]}>
+        <img src={imagenes[i]} alt={`proyecto ${i}`} />
+        <span>{titulos[i]}</span>
+      </a>
+    </li>,
+  )
+}
+
 const Projects = () => (
-  <div id="projects">Esto es un proyecto</div>
+  <div id="projects">
+    <h1>Proyectos</h1>
+    <ul>{elementos}</ul>
+  </div>
 )
 
 export default Projects
